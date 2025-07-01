628744bde8db9d0edd3913c396a6d5e27c8f1ef3
BuildTools <unconfigured@null.spigotmc.org>
2025-07-01 14:53:04 +0900

    from Mochidsuki Main PC fixed JumpPad Bug (Launch in Spectator)

--- src/main/java/jp/houlab/mochidsuki/elytra_jetpacker/JumpPad.java
+++ src/main/java/jp/houlab/mochidsuki/elytra_jetpacker/JumpPad.java
@@ -1,9 +1,6 @@
 package jp.houlab.mochidsuki.elytra_jetpacker;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -29,7 +26,7 @@ public class JumpPad extends BukkitRunnable {
             for(Player player : loc.getWorld().getPlayers()) {
                 int x = player.getLocation().getBlockX();
                 int z = player.getLocation().getBlockZ();
-                if(player.getLocation().distance(loc) <= 3 && x == loc.getBlockX() && z == loc.getBlockZ()) {
+                if(!player.getGameMode().equals(GameMode.SPECTATOR) && player.getLocation().distance(loc) <= 3 && x == loc.getBlockX() && z == loc.getBlockZ()) {
                     player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,JumpPadLocation.get(loc)[0]*20, JumpPadLocation.get(loc)[1],true,false,false));
                     player.addScoreboardTag("JetPack");
                     new RemoveTag(player).runTaskTimer(plugin,20L,1);
